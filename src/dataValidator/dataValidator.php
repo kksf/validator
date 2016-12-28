f393b823323f685588c364ed25e836704c712906
Kiril Kapustin <kiril.kapustin@gmail.com>
2016-12-28 19:29:04 +0200

    init

---
+++ src/dataValidator/dataValidator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace pillax;
+
+class dataValidator {
+
+    public static function validate($value, $rule) {
+        if( ! method_exists('self', $rule)) {
+            throw new \Exception('Invalid validation rule');
+        }
+
+        return self::$rule($value);
+    }
+
+    public static function isNumeric($value) {
+        return is_numeric($value);
+    }
+
+    public static function isFloat($value) {
+        return is_float($value);
+    }
+
+    public static function isString($value) {
+        return is_string($value);
+    }
+}
